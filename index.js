edeacab0f2fa12775d396e6d0a7cfbd4239a47c8
Rajkumar-pr <139167030+Rajkumar-pr@users.noreply.github.com>
2025-09-10 01:17:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const feedUpdate=require("./controller/Admin");
 dotenv.config(); // load env variables
 
 const app = express();
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 const router = require("./routes/SignRoute");
 const router1 = require("./routes/OrderRoute");
 const router2 = require("./routes/Feedback");
-
+const router4=require("./routes/Admin");
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -26,6 +26,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/", router);
 app.use("/", router2);
 app.use("/order", router1);
+app.use("/Feed",router4);
 
 // Test route
 app.get("/", (req, res) => {
@@ -35,3 +36,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`App is listening on PORT: ${PORT}`);
 });
+
